dd76d8fc27ce87ba79a813ad21965085ea284693
Siyuan <87063252+xingsy97@users.noreply.github.com>
2024-01-17 02:33:30 +0000

    update

--- Program.cs
+++ Program.cs
@@ -5,9 +5,13 @@ public class SamplesCultureInfo
 {
     public static void Main()
     {
+        Console.WriteLine(".NET Version: " + Environment.Version);
+
         CultureInfo validCultureInfo = new CultureInfo("en-US", false);
-        Console.WriteLine(validCultureInfo.Name);
+        Console.WriteLine("Valid Culture = " + validCultureInfo.Name);
+
+        // SHOULD throw exception. Ref https://learn.microsoft.com/en-us/dotnet/api/system.globalization.cultureinfo.-ctor?view=net-8.0&viewFallbackFrom=dotnet-aspire-8.0#system-globalization-cultureinfo-ctor(system-string)
         CultureInfo invalidCultureInfo = new CultureInfo("123", false);
-        Console.WriteLine(invalidCultureInfo.Name);
+        Console.WriteLine("Invalid Culture = " + invalidCultureInfo.Name);
     }
 }
