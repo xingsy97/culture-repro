d577d24e993335b8781bf72cf86d4e9c22a7f16c
xingsy97 <87063252+xingsy97@users.noreply.github.com>
2024-01-17 10:28:20 +0800

    init

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Globalization;
+
+public class SamplesCultureInfo
+{
+    public static void Main()
+    {
+        CultureInfo validCultureInfo = new CultureInfo("en-US", false);
+        Console.WriteLine(validCultureInfo.Name);
+        CultureInfo invalidCultureInfo = new CultureInfo("123", false);
+        Console.WriteLine(invalidCultureInfo.Name);
+    }
+}
